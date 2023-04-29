62c03c677095cb12d95fa47066ccaebed9ec9442
Michael <michaael.monir119@gmail.com>
2023-04-29 23:59:19 +0200

    Added shortest paths algorithms and heading for testing them

---
+++ src/main/java/org/example/GraphTest.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import static org.junit.Assert.*;
+
+public class GraphTest
+{
+
+    @org.junit.Test
+    public void getSize() {
+    }
+
+    @org.junit.Test
+    public void dijkstra() {
+    }
+
+    @org.junit.Test
+    public void bellmanFord() {
+    }
+
+    @org.junit.Test
+    public void floydWarshall() {
+    }
+
+    @org.junit.Test
+    public void hasNegativeCycle() {
+    }
+}
